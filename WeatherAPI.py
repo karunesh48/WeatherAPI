510316574afde43a660e272a0161ad9305f7bbb6
karunesh48 <113983456+karunesh48@users.noreply.github.com>
2025-06-02 13:43:41 +0530

    WeatherAPI.py

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,25 +1,11 @@
+import date and time
 import requests
 
-def get_weather(city_name, api_key):
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        'q': city_name,
-        'appid': api_key,
-        'units': 'metric'
-    }
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+API_KEY  = open('2e8c672a07d6298cb6933578ff606cd7',r).read()
+CITY = "London"
 
-    response = requests.get(base_url, params=params)
+url = BASE_URL + "appid=" + API_KEY +"&q=" + CITY
 
-    if response.status_code == 200:
-        data = response.json()
-        print(f"Weather in {city_name}:")
-        print(f"Temperature: {data['main']['temp']} °C")
-        print(f"Condition: {data['weather'][0]['description'].title()}")
-        print(f"Humidity: {data['main']['humidity']}%")
-    else:
-        print("Error fetching data:", response.status_code, response.text)
-
-# Replace with your actual API key
-API_KEY = "2e8c672a07d6298cb6933578ff606cd7"
-city = input("Enter a city name: ")
-get_weather(city, API_KEY)
+response = requests.get(url).json()
+print(response)
